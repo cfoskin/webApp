2234d3382be0d087911ff01efd75040fd125fe2a
cfoskin <columfoskin@gmail.com>
2015-10-20 13:31:06 +0100

    removed extra script tags

--- myApp/app.js
+++ myApp/app.js
@@ -1,54 +1,16 @@
-app.run(function ($rootScope, $state, loginModal) {
-  $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-    var requireLogin = toState.data.requireLogin;
+var eCommerceApp = angular.module('eCommerceApp', ['ngRoute']);
 
-    if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
-      event.preventDefault();
-
-      loginModal()
-        .then(function () {
-          return $state.go(toState.name, toParams);
-        })
-        .catch(function () {
-          return $state.go('welcome');
-        });
-    }
-  });
-});
-
-app.config(function ($httpProvider) {
-
-  $httpProvider.interceptors.push(function ($timeout, $q, $injector) {
-    var loginModal, $http, $state;
-
-    // this trick must be done so that we don't receive
-    // `Uncaught Error: [$injector:cdep] Circular dependency found`
-    $timeout(function () {
-      loginModal = $injector.get('loginModal');
-      $http = $injector.get('$http');
-      $state = $injector.get('$state');
-    });
-
-    return {
-      responseError: function (rejection) {
-        if (rejection.status !== 401) {
-          return rejection;
-        }
-
-        var deferred = $q.defer();
-
-        loginModal()
-          .then(function () {
-            deferred.resolve( $http(rejection.config) );
+eCommerceApp.config(['$routeProvider',
+      function($routeProvider) {
+        $routeProvider
+          .when('/views', {
+            templateUrl: './partials/grid.html',
           })
-          .catch(function () {
-            $state.go('welcome');
-            deferred.reject(rejection);
-          });
-
-        return deferred.promise;
-      }
-    };
-  });
-
-});
+          // .when('/phones/:phoneId', {
+          //   templateUrl: 'partials/phone-detail.html',
+          //   controller: 'PhoneDetailCtrl'
+          // })
+          .otherwise({
+            redirectTo: '/views'
+          })
+      }])
