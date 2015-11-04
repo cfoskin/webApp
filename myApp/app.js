b28f54249cee88b47e5b57672d1241e12f9eb973
cfoskin <columfoskin@gmail.com>
2015-11-04 21:00:04 +0000

    added shopping cart and orders

--- myApp/app.js
+++ myApp/app.js
@@ -3,48 +3,53 @@ var eCommerceApp = angular.module('eCommerceApp', ['ngRoute']);
 eCommerceApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
-    .when('/home', {
+    .when('/', {
       templateUrl: '/myApp/views/partials/homePage.html',
-      controller:  'homePageController' 
+      controller:  'HomePageController' 
     })
     .when('/products', {
       templateUrl: '/myApp/views/partials/products.html',
       controller: 'ProductPageController'
     })
+    .when('/phones/:phoneId', {
+      templateUrl: '/myApp/views/partials/phone-detail.html',
+      controller: 'PhoneDetailCtrl'
+    })
     .when('/register', {
       templateUrl: '/myApp/views/partials/register.html',
-      controller:  'registerController'
+      controller:  'RegisterController'
 
     })
     .when('/shoppingCart', {
       templateUrl: '/myApp/views/partials/shoppingCart.html',
-      controller: 'shoppingCartController'
+      controller: 'ShoppingCartController'
     })
     .when('/account', {
       templateUrl: '/myApp/views/partials/account.html',
-      controller:  'accountController',
+      controller:  'AccountController',
+    })
+    .when('/checkOut', {
+      templateUrl: '/myApp/views/partials/checkOut.html',
+      controller:  'CheckOutController'
     })
     .otherwise({
-      redirectTo: '/home'
+      redirectTo: '/'
     })
 
     // use the HTML5 History API
     $locationProvider.html5Mode(true);
   }]);
 
-//A service for storing user Authentication
+//A service for storing user User Authentication
 eCommerceApp.service('AuthenticationService', function (StorageService) {
   this.loggedInUser = null;
   isUserLoggedIn = false;
-
-  debugger;
   var users = StorageService.getUsers();
-  
   this.getUsers = function () {
     return users;
   }
 
-  this.addUser = function(userData) {
+  this.registerUser = function(userData) {
    users.push(new User(userData));
    StorageService.saveUsers(users);
  }
@@ -72,7 +77,7 @@ this.updateUser = function(user) {
 //A service for storing users to local storage
 eCommerceApp.service('StorageService', function () {
   this.getUsers = function() {
-    var users = JSON.parse(localStorage.getItem('users'))  || [];
+    var users = JSON.parse(localStorage.getItem('users'))  || [{name: 'colum', lastName:'foskin', address: 'waterford', email:'colum@foskin.com', password:'pass'}];
     return users.map(function(userData) {
       return new User(userData);
     });
@@ -83,20 +88,41 @@ eCommerceApp.service('StorageService', function () {
   }
 })
 
+
 //A service for the phones
 eCommerceApp.factory('PhoneService', ['$http' , function($http){
+  var phones =[];
+  $http.get('/myApp/phones/phones.json').success(function(phoneData) {
+    phoneData.forEach(function(data) {
+      phones.push(new Phone(data));
+    });
+  })
   var api = {
     getPhones : function() {
-      return $http.get('/myApp/phones/phones.json')
+      return phones
+    },
+    addToCart : function(phone, user){
+      user.addItemToCart(phone)
+    },
+    getPhone : function(id){
+      return phones.filter(function(phone){
+        return phone.id === id;
+      }).pop();
+    },
+    loadPhone : function(id){
+      return $http.get('/myApp/phones/' + id + '.json');
     }
+
   }
-  return api
+  return api;
 }]);
 
 
-eCommerceApp.controller('homePageController', 
+eCommerceApp.controller('HomePageController', 
   function ($scope, $location, AuthenticationService,PhoneService) {
     $scope.logIn = {};
+    //REMOVE AT END!!!!!!
+    AuthenticationService.loggedInUser = AuthenticationService.getUsers()[0];
     $scope.logIn = function () {
       AuthenticationService.logIn($scope.logIn.email, $scope.logIn.password );
       if (AuthenticationService.loggedInUser){
@@ -104,65 +130,130 @@ eCommerceApp.controller('homePageController',
       }
     }
 
-    PhoneService.getPhones().success(function(data) {
-     $scope.phones = data
-     $scope.phones.map(function(phoneData) {
-      return new Phone(phoneData);
-    });
-   })
+    $scope.phones = PhoneService.getPhones();
     $scope.orderProp = 'age';
     $scope.quantity = 2;
   });
 
-
-eCommerceApp.controller('ProductPageController', 
-  function ($scope,$location,PhoneService, AuthenticationService) {   
-   PhoneService.getPhones().success(function(data) {
-     $scope.phones = data
-
-     $scope.phones.map(function(phoneData) {
-      return new Phone(phoneData);
-    });
-
-   })
-   $scope.orderProp = 'age';
-   $scope.loggedInUser = AuthenticationService.loggedInUser;
- });
-
-
-eCommerceApp.controller('shoppingCartController', 
- function ($scope,$location,AuthenticationService) {
-  $scope.loggedInUser = AuthenticationService.loggedInUser;
-  if( $scope.loggedInUser === null){
-    $location.path('/')
-  }
-});
-
-eCommerceApp.controller('accountController', 
+eCommerceApp.controller('AccountController', 
  function ($scope,$location,$routeParams, AuthenticationService) {
   $scope.loggedInUser = AuthenticationService.loggedInUser;
   if( $scope.loggedInUser === null){
-    $location.path('/')
-  }
-
-  $scope.logOut = function () {
-    AuthenticationService.logOut();
-    $location.path('/')
-  }
-
-  $scope.updateUser = function () {
-   AuthenticationService.updateUser($scope.loggedInUser);
-   this.logOut();
+   alert("Please log in or create and account to view your account page!");
+   $location.path('/')
  }
+
+ $scope.logOut = function () {
+  AuthenticationService.logOut();
+  $location.path('/')
+}
+
+$scope.updateUser = function () {
+ AuthenticationService.updateUser($scope.loggedInUser);
+ this.logOut();
+}
 });
 
-eCommerceApp.controller('registerController', function ($scope,AuthenticationService) {
+eCommerceApp.controller('RegisterController', function ($scope,AuthenticationService,$location) {
   $scope.users = AuthenticationService.getUsers();
-  $scope.addUser = function () {
-    AuthenticationService.addUser($scope.newUser);
+  $scope.registerUser = function () {
+    AuthenticationService.registerUser($scope.newUser);
     $scope.newUser = new User({});
+    $location.path('/')
   }
 });
+
+eCommerceApp.controller('ProductPageController', 
+  function ($scope,$location,PhoneService, AuthenticationService) {     
+    $scope.phones = PhoneService.getPhones();
+
+    $scope.loggedInUser = AuthenticationService.loggedInUser;
+    $scope.addToCart = function($event){
+      var phone = PhoneService.getPhone($event.target.id)
+      PhoneService.addToCart(phone,$scope.loggedInUser);
+    }
+    $scope.orderProp = 'age';
+  });
+
+eCommerceApp.controller('PhoneDetailCtrl', 
+ ['$scope', '$location', '$routeParams', 'PhoneService', 
+ function($scope, $location, $routeParams, PhoneService) {
+    PhoneService.loadPhone($routeParams.phoneId).success(function(data) {
+      $scope.phone = data;
+      $scope.img = $scope.phone.images[0]
+    })
+    
+
+   $scope.setImage = function(img) {
+    $scope.img = img
+  }
+}]);
+
+
+eCommerceApp.controller('ShoppingCartController', 
+  function ($scope,$location,AuthenticationService,PhoneService) {
+    $scope.loggedInUser = AuthenticationService.loggedInUser;
+    if( $scope.loggedInUser === null){
+      alert("Please log in or create and account to view your shopping cart!");
+      $location.path('/')
+    }
+    $scope.removeFromCart=  function($event){
+      var phone = PhoneService.getPhone($event.target.id);
+      $scope.loggedInUser.removeItemFromCart(phone);
+    }
+
+    $scope.checkOut = function(){
+      $scope.loggedInUser.myCartItems.length
+      if($scope.loggedInUser.myCartItems.length == 0){
+        alert("Please add items to your cart first!");
+        $location.path("/products")
+      }
+      else{
+        var cartTotal = $scope.loggedInUser.calculateCartTotal();
+        if($scope.loggedInUser.checkOut($scope.loggedInUser.myCartItems, cartTotal)){
+          $location.path('/checkOut');
+        }
+      }
+    }
+  });
+
+eCommerceApp.directive('navbarDirective', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: './partials/navbar.html'
+  }
+});
+
+eCommerceApp.directive('footerDirective', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: './partials/footer.html'
+  }
+});
+
+eCommerceApp.controller('CheckOutController', 
+  function ($scope,$location,AuthenticationService,PhoneService) {
+    $scope.loggedInUser = AuthenticationService.loggedInUser;
+    if( $scope.loggedInUser === null){
+      $location.path('/')
+    }
+    var orders = $scope.loggedInUser.myOrders;
+    var incompleteOrder = null;
+    orders.forEach(function(order) {
+     if(order.isCompleted === false){
+      incompleteOrder = order;
+    }
+  });
+
+    $scope.completeOrder = function(){
+      if($scope.loggedInUser.completeOrder(incompleteOrder, incompleteOrder.orderPrice)){
+        alert("Order Completed");
+        $location.path('/account');
+      }
+    }
+
+  });
+
 
 
 function User(data) {
@@ -171,19 +262,61 @@ function User(data) {
   this.email = data.email || '',
   this.address = data.address || '',
   this.password = data.password || '',
-  this.myCartItems = null || [],
-
+  this.myCartItems = data.myCartItems || [],
+  this.myOrders = data.myOrders || [],
 
   this.yourCredentials = function(email, password) {
     return email === this.email && password === this.password;
   }
-}
 
+  this.addItemToCart = function(phone){
+    return this.myCartItems.push(phone);
+  }
+
+  this.removeItemFromCart = function(phone){
+    var index = this.myCartItems.indexOf(phone);
+    return this.myCartItems.splice(index, 1);
+  }
+
+  this.calculateCartTotal = function(){
+    var total =0;
+    this.myCartItems.forEach(function(phone) {
+      total += phone.price;
+    })
+    return total;
+  }
+
+  this.checkOut = function(cartItems, cartTotal){
+   var orderIndex = this.myOrders.length;
+   debugger;
+   var order = new Order(cartItems, cartTotal, orderIndex, false);
+   this.myCartItems = [];
+   return this.myOrders.push(order);
+ }
+
+ this.completeOrder = function(incompleteOrder){
+   incompleteOrder.orderStatus = 'Completed & Shipped';
+   return incompleteOrder.isCompleted = true;
+ }
+}
 
 function Phone(data) {
   this.age = data.age || '',
   this.id = data.id || '',
+  this.price = data.price || '',
   this.imageUrl = data.imageUrl || '',
   this.name = data.name || '',
   this.snippet = data.snippet || ''
+}
+
+function Order(orderedProduct, orderPrice, orderNum, isCompleted) {
+  this.orderedProduct = orderedProduct,
+  this.isCompleted = isCompleted,
+  this.orderPrice = orderPrice,
+  this.orderNum = orderNum,
+  this.orderStatus = 'Incomplete'
+
+
+
 };
+
