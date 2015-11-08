88be2a0a096a035e4e9f23cc8f6cb0c140cc4dd2
cfoskin <columfoskin@gmail.com>
2015-11-08 15:22:53 +0000

    final commit

--- myApp/app.js
+++ myApp/app.js
@@ -1,45 +1,47 @@
-var eCommerceApp = angular.module('eCommerceApp', ['ngRoute']);
+//ecommerce app by Colum Foskin 20062042, Component Development, Applied Computing.
 
+var eCommerceApp = angular.module('eCommerceApp', ['ngRoute']);
+//routes
 eCommerceApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/', {
-      templateUrl: '/myApp/partials/homePage.html',
+      templateUrl: '/partials/homePage.html',
       controller:  'HomePageController' 
     })
     .when('/products', {
-      templateUrl: '/myApp/partials/products.html',
+      templateUrl: '/partials/products.html',
       controller: 'ProductPageController'
     })
     .when('/landingPage', {
-      templateUrl: '/myApp/partials/landingPage.html',
+      templateUrl: '/partials/landingPage.html',
     })
     .when('/phones/:phoneId', {
-      templateUrl: '/myApp/partials/phone-detail.html',
+      templateUrl: '/partials/phone-detail.html',
       controller: 'PhoneDetailCtrl'
     })
     .when('/register', {
-      templateUrl: '/myApp/partials/register.html',
+      templateUrl: '/partials/register.html',
       controller:  'RegisterController'
 
     })
     .when('/shoppingCart', {
-      templateUrl: '/myApp/partials/shoppingCart.html',
+      templateUrl: '/partials/shoppingCart.html',
       controller: 'ShoppingCartController'
     })
     .when('/account', {
-      templateUrl: '/myApp/partials/account.html',
+      templateUrl: '/partials/account.html',
       controller:  'AccountController',
     })
     .when('/completeOrder', {
-      templateUrl: '/myApp/partials/completeOrder.html',
+      templateUrl: '/partials/completeOrder.html',
       controller:  'CompleteOrderController'
     })
     .otherwise({
       redirectTo: '/'
     })
 
-    // use the HTML5 History API
+    // use the HTML5 History API to remove the # from url
     $locationProvider.html5Mode(true);
   }]);
 
@@ -47,6 +49,7 @@ eCommerceApp.config(['$routeProvider', '$locationProvider',
 eCommerceApp.service('AuthenticationService', function (StorageService, $location) {
   this.loggedInUser = null;
   var users = StorageService.getUsers();
+
   this.getUsers = function () {
     return users;
   }
@@ -63,18 +66,15 @@ this.checkAlreadyReg = function(userData){
   })
   return result;
 }
-
 this.registerUser = function(userData) {
  users.push(new User(userData));
-    // StorageService.saveUsers(users);
+ StorageService.saveUsers(users);
   }
-
-  this.logIn = function(email, password) {
+  this.signIn = function(email, password) {
     users.forEach(function(user) {
       if(user.yourCredentials(email, password))
         this.loggedInUser = user;
     }.bind(this));
-
   }
   this.signOut = function() {
    this.loggedInUser = null;
@@ -82,29 +82,25 @@ this.registerUser = function(userData) {
  this.updateUser = function(user, newPassword) {
   user.password = newPassword;
 }
-
 });
-
 
 //A service for storing users to local storage
 eCommerceApp.service('StorageService', function () {
   this.getUsers = function() {
     var users = JSON.parse(localStorage.getItem('users'))  || [{name: 'colum', lastName:'foskin', address: 'waterford', email:'colum@foskin.com', password:'pass'}];
-    return users.map(function(userData) {
+    return users.map(function(userData) {//create User objects from the users stored in json
       return new User(userData);
     });
-  },
-
+  }
   this.saveUsers = function(users) {
     localStorage.setItem('users', JSON.stringify(users));
   }
 })
 
-
 //A service for the phones
 eCommerceApp.factory('PhoneService', ['$http' , function($http){
   var phones =[];
-  $http.get('/myApp/phones/phones.json').success(function(phoneData) {
+  $http.get('/phones/phones.json').success(function(phoneData) {
     phoneData.forEach(function(data) {
       phones.push(new Phone(data));
     });
@@ -122,23 +118,20 @@ eCommerceApp.factory('PhoneService', ['$http' , function($http){
       }).pop();
     },
     loadPhone : function(id){
-      return $http.get('/myApp/phones/' + id + '.json');
+      return $http.get('/phones/' + id + '.json');
     }
-
   }
   return api;
 }]);
 
-
+//the controller for the homepage which has only logging in.
 eCommerceApp.controller('HomePageController', 
   function ($scope,$rootScope, $location, AuthenticationService) {
-    $scope.logIn = {};
-    AuthenticationService.loggedInUser = false;
+    $scope.signIn = {};
+    AuthenticationService.loggedInUser = false;//using this as a flag for ng-show
     $rootScope.showAccount =  AuthenticationService.loggedInUser;
-   // AuthenticationService.loggedInUser = AuthenticationService.getUsers()[0];
-   $scope.logIn = function () {
-        AuthenticationService.loggedInUser = null;//log out old
-        AuthenticationService.logIn($scope.logIn.email, $scope.logIn.password );
+   $scope.signIn = function () {
+        AuthenticationService.signIn($scope.signIn.email, $scope.signIn.password );
         if (AuthenticationService.loggedInUser){
           $rootScope.showAccount = true;
           $location.path('/account');
@@ -149,17 +142,18 @@ eCommerceApp.controller('HomePageController',
       }
     });
 
+//The account controller which allows the user to finish incomplete orders, and update 
+//their password
 eCommerceApp.controller('AccountController', 
   function ($scope,$location, AuthenticationService, PhoneService) {
     $scope.loggedInUser = AuthenticationService.loggedInUser;
     $scope.phones = PhoneService.getPhones();
     $scope.orderProp = 'age';
     $scope.quantity = 2;
-    $scope.warning = false;
+    $scope.warning = false;//ng-show flag
 
     $scope.completeOrder=  function(){
       $location.path("/completeOrder");
-
     }
     $scope.updateUser = function () {
       if($scope.password1 === $scope.password2){
@@ -173,7 +167,7 @@ eCommerceApp.controller('AccountController',
     }
   });
 
-
+//controller which is used by the navbar and footer which both have sign out feature
 eCommerceApp.controller('SignOutController', 
  function ($scope,$location, AuthenticationService) {
   $scope.signOut = function () {
@@ -181,9 +175,9 @@ eCommerceApp.controller('SignOutController',
     alert("You are now signed out")
     $location.path('/')
   }
-
 });
 
+//directive for navbar
 eCommerceApp.directive('navbarDirective', function() {
   return {
     restrict: 'AE',
@@ -192,8 +186,7 @@ eCommerceApp.directive('navbarDirective', function() {
   }
 });
 
-
-
+//directive for footer
 eCommerceApp.directive('footerDirective', function() {
   return {
     restrict: 'AE',
@@ -202,25 +195,28 @@ eCommerceApp.directive('footerDirective', function() {
   }
 });
 
+//registration controller
 eCommerceApp.controller('RegisterController', function ($scope,AuthenticationService,$location) {
   $scope.users = AuthenticationService.getUsers();
-  $scope.warning = false;
+  $scope.warning = false;//ng-show flag
   $scope.registerUser = function () {
     if(AuthenticationService.checkAlreadyReg($scope.newUser)){
       $scope.warning = true;
     }
     else{
       AuthenticationService.registerUser($scope.newUser);
+      alert("Success! Please Sign In");
       $location.path('/');
     }
   }
 });
 
+//product page controller which allows a use to add an item to their cart
 eCommerceApp.controller('ProductPageController', 
   function ($scope,$location,PhoneService, AuthenticationService) {     
     $scope.phones = PhoneService.getPhones();
     $scope.loggedInUser = AuthenticationService.loggedInUser;
-    $scope.warning = false;
+    $scope.warning = false;//ng-show flag
     $scope.addToCart = function($event){
       if($scope.loggedInUser){
         var phone = PhoneService.getPhone($event.target.id)
@@ -233,6 +229,7 @@ eCommerceApp.controller('ProductPageController',
     $scope.orderProp = 'age';
   });
 
+//controller for the individual phone detail page
 eCommerceApp.controller('PhoneDetailCtrl', 
  ['$scope', '$location', '$routeParams', 'PhoneService', 
  function($scope, $location, $routeParams, PhoneService) {
@@ -240,18 +237,16 @@ eCommerceApp.controller('PhoneDetailCtrl',
     $scope.phone = data;
     $scope.img = $scope.phone.images[0]
   })
-
-
   $scope.setImage = function(img) {
     $scope.img = img
   }
 }]);
 
-
+//shopping cart controller for removing items or creating an order from the cart
 eCommerceApp.controller('ShoppingCartController', 
   function ($scope,$location,AuthenticationService,PhoneService) {
     $scope.loggedInUser = AuthenticationService.loggedInUser;
-    $scope.warning = false;
+    $scope.warning = false;//ng-show flag
     if( $scope.loggedInUser === null){
       alert("Please log in or create and account to view your shopping cart!");
       $location.path('/')
@@ -260,20 +255,18 @@ eCommerceApp.controller('ShoppingCartController',
       var phone = PhoneService.getPhone($event.target.id);
       $scope.loggedInUser.removeItemFromCart(phone);
     }
-
     $scope.createOrder = function(){
-     // $scope.loggedInUser.myCartItems.length
-     if(!$scope.loggedInUser.shoppingCart.hasItems()){
+     if(!$scope.loggedInUser.shoppingCart.hasItems()){//check for empty cart
       $scope.warning = true;
     }
     else{
-      if($scope.loggedInUser.createOrder()){
-        $location.path('/completeOrder');
-      }
+      $scope.loggedInUser.createOrder();
+      $location.path('/completeOrder');
     }
   }
 });
 
+//controller to allow the use finish off the order process
 eCommerceApp.controller('CompleteOrderController', 
   function ($scope,$location,AuthenticationService,PhoneService) {
     $scope.loggedInUser = AuthenticationService.loggedInUser;
@@ -284,17 +277,15 @@ eCommerceApp.controller('CompleteOrderController',
       incompleteOrder = order;
     }
   });
-
     $scope.completeOrder = function(){
-      if($scope.loggedInUser.completeOrder(incompleteOrder, incompleteOrder.orderPrice)){
+      if($scope.loggedInUser.completeOrder(incompleteOrder)){
         alert("Order Completed");
         $location.path('/account');
       }
     }
-
   });
 
-
+//the User model
 function User(data) {
   this.name = data.name || '',
   this.lastName = data.lastName || '',
@@ -307,30 +298,26 @@ function User(data) {
   this.yourCredentials = function(email, password) {
     return email === this.email && password === this.password;
   }
-
   this.addItemToCart = function(phone){
-    return this.shoppingCart.add(phone);
+    this.shoppingCart.add(phone);
   }
-
   this.removeItemFromCart = function(phone){
-   
-    return this.shoppingCart.remove(phone);
+    this.shoppingCart.remove(phone);
   }
-
   this.createOrder = function(){
     var cartTotal = this.shoppingCart.calculateTotal();
     var orderIndex = this.myOrders.length +1;
     var order = new Order(this.shoppingCart.getItems(), cartTotal, orderIndex, false);
     this.shoppingCart.emptyCart();
-    return this.myOrders.push(order);
+    this.myOrders.push(order);
   }
-
   this.completeOrder = function(incompleteOrder){
    incompleteOrder.orderStatus = 'Completed & Shipped';
    return incompleteOrder.isCompleted = true;
  }
 }
 
+//the Phone model
 function Phone(data) {
   this.age = data.age || '',
   this.id = data.id || '',
@@ -340,17 +327,16 @@ function Phone(data) {
   this.snippet = data.snippet || ''
 }
 
-function Order(orderedProducts, orderPrice, orderNum, isCompleted) {
+//the Order model
+function Order(orderedProducts, orderTotal, orderNum, isCompleted) {
   this.orderedProducts = orderedProducts,
   this.isCompleted = isCompleted,
-  this.orderPrice = orderPrice,
+  this.orderTotal = orderTotal,
   this.orderNum = orderNum,
   this.orderStatus = 'Incomplete'
-
 };
 
-
-
+//the shopping cart model
 function ShoppingCart(){
   this.items = [],
 
@@ -361,26 +347,23 @@ function ShoppingCart(){
     })
     return total;
   }
-
   this.add = function(phone){
-    this.items.push(phone);
+    var index = this.items.indexOf(phone);
+    if(index < 0){
+          this.items.push(phone);
+    }
   }
-
   this.remove = function(phone){
     var index = this.items.indexOf(phone);
-    return this.items.splice(index, 1);
+    this.items.splice(index, 1);
   }
-
   this.emptyCart = function(){
     this.items = [];
   }
-
   this.getItems =function(){
     return this.items;
   }
-
   this.hasItems = function() {
     return this.items.length > 0;
   }
-
 }
