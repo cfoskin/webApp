b17d7ba249870fabc57d5a03cfa2ec91da2f6db5
cfoskin <columfoskin@gmail.com>
2015-12-05 10:52:52 +0000

    added nested documents for user and order

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
+var PhoneSchema = new Schema({
+	name: { type: String, required: true },
+	price: { type:Number, required: false }
+});
+
 var OrderSchema = new Schema({
 	total: { type: Number, required: false },
 	dispatchStatus: { type: String, required: true },
-	status: { type: String, required: true },
+	orderedProducts:[PhoneSchema]
 });
 
 var UserSchema = new Schema({
